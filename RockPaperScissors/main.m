e3e270eca7e89977344ce1af3d239d7df032bfe8
Daniel Pratt <daniel@blaumagier.com>
2017-05-03 11:19:02 -0700

    Initial Commit

---
+++ RockPaperScissors/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RockPaperScissors
+//
+//  Created by Daniel Pratt on 5/3/17.
+//  Copyright © 2017 Daniel Pratt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
